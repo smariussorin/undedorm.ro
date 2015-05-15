cfdb20b42c9f446be79c5b3bd62ea884b64f0b17
Radu Vlad <vlar.radu.mps@gmail.com>
2015-05-15 22:28:10 +0300

    Update footer and fix js main

--- js/main.js
+++ js/main.js
@@ -175,7 +175,6 @@ $(window).load(function() {
     ------------------------------*/
     $('.sliderCont').flexslider({
         animation: "fade",
-		itemWidth: 111,
         // Primary Controls
         controlNav: false,               //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
         directionNav: true,             //Boolean: Create navigation for previous/next navigation? (true/false)
@@ -184,7 +183,6 @@ $(window).load(function() {
     });
     $('.sliderCont2').flexslider({
         animation: "fade",
-		  itemWidth: 111,
         // Primary Controls
         controlNav: "thumbnails",       //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
         directionNav: true,             //Boolean: Create navigation for previous/next navigation? (true/false)
